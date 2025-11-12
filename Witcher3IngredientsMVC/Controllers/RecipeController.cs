00d6dd929e264102c641a2d85de0b05c45cb870c
asd asd <seonghyejin33@gmail.com>
2025-11-12 16:09:25 +0700

    recipe finished

--- Witcher3IngredientsMVC/Controllers/RecipeController.cs
+++ Witcher3IngredientsMVC/Controllers/RecipeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Witcher3IngredientsMVC.Data;
+using Witcher3IngredientsMVC.Models;
 using Witcher3IngredientsMVC.Models.ViewModels;
 
 namespace Witcher3IngredientsMVC.Controllers
@@ -91,6 +92,102 @@ namespace Witcher3IngredientsMVC.Controllers
 
             return View( viewModel);
         }
+        [HttpPost]
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> Edit(RecipeUpsertViewModel model)
+        {
+            if (!model.Requirements.Any(r => r.ItemId != 0 && r.Quantity > 0))
+            {
+                var items = await dbContext.Items
+    .Select(i => new SelectListItem { Value = i.Id.ToString(), Text = i.Name })
+    .ToListAsync();
 
+                foreach (var req in model.Requirements)
+                    req.AvailableItems = items;
+                ModelState.AddModelError("", "Please add at least one requirement.");
+                return View("Edit", model);
+            }
+            if (!ModelState.IsValid)
+            {
+                var items = await dbContext.Items
+    .Select(i => new SelectListItem { Value = i.Id.ToString(), Text = i.Name })
+    .ToListAsync();
+
+                foreach (var req in model.Requirements)
+                    req.AvailableItems = items;
+                return View(model);
+
+            }
+            if (model.Id != 0)
+            {
+
+            
+            var recipe = await dbContext.Recipes
+                .Include(r => r.Requirements)
+                .FirstOrDefaultAsync(r => r.Id == model.Id);
+
+            if (recipe == null)
+                return NotFound();
+
+            // Update basic fields
+            recipe.Name = model.Name;
+
+            // Remove old requirements
+            dbContext.RecipeRequirements.RemoveRange(recipe.Requirements);
+
+            // Add new ones from the ViewModel
+            recipe.Requirements = model.Requirements
+                .Where(r => r.ItemId != 0 && r.Quantity > 0) // skip empty rows
+                .Select(r => new RecipeRequirement
+                {
+                    RecipeId = recipe.Id,
+                    ItemId = r.ItemId,
+                    Quantity = r.Quantity
+                })
+                .ToList();
+            }
+            else
+            {
+                var created = new Recipe
+                {
+                    Name = model.Name,
+                    Requirements= model.Requirements
+                .Where(r => r.ItemId != 0 && r.Quantity > 0) // skip empty rows
+                .Select(r => new RecipeRequirement
+                {
+                   
+                    ItemId = r.ItemId,
+                    Quantity = r.Quantity
+                })
+                .ToList()
+                };
+                await dbContext.Recipes.AddAsync(created);
+            }
+                await dbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var recipe = await dbContext.Recipes
+
+         .Include(x => x.Requirements)
+             .ThenInclude(link => link.Item)
+
+        .FirstOrDefaultAsync(i => i.Id == id);
+
+            if (recipe != null)
+            {
+                // Remove related ItemLinks first
+
+                dbContext.RecipeRequirements.RemoveRange(recipe.Requirements);
+
+                dbContext.Recipes.Remove(recipe);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
